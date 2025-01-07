faaa383af8fbf3466fe381b033c2fe8fe7739cdb
Ferry To <ferrywlto@gmail.com>
2025-01-07 23:07:13 +0000

    ui: Change blog title and author in footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '⛴️ Ferry\'s Blog 😎';
+export const SITE_DESCRIPTION = '🐹 Welcome to my website! 🐈';
