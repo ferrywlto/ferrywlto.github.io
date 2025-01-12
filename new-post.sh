bb400da757f3188fa5c1c70ad3649fd69fb9ab0b
Ferry To <ferrywlto@gmail.com>
2025-01-12 21:50:02 +0000

    feat: Add script to generate new post template

---
+++ new-post.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Generate the current date in yyyyMMdd format
+current_date=$(date +"%Y-%m-%d")
+path="src/content/blog/dev/latest/"
+filename="${current_date}.md"
+fullpath="${path}${filename}"
+
+# Create a new file with the date as the filename
+touch $fullpath
+echo "---" >> $fullpath
+echo "title: ''" >> $fullpath
+echo "description: ''" >> $fullpath
+echo "pubDate: ${current_date}" >> $fullpath
+echo "tags: []" >> $fullpath
+echo "---" >> $fullpath
+echo "Created file: ${fullpath}"
+
