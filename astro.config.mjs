7c708061c951943a842c4fae8ee3f41e868830a5
Ferry To <ferrywlto@gmail.com>
2025-01-26 22:10:56 +0000

    feat: Add AlpineJS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ferrywl.to',
-	integrations: [mdx(), sitemap()],
+    site: 'https://ferrywl.to',
+    integrations: [mdx(), sitemap(), alpinejs()],
 });
