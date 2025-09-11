92185046edb25541150c031242bf32726ed640c9
Ferry To <ferrywlto@gmail.com>
2025-09-11 21:22:18 +0100

    feat: Add new collections for new layout.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,9 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+const blogs = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blogs/` directory.
+	loader: glob({ base: './src/content/blogs', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
@@ -26,4 +26,31 @@ const tweets = defineCollection({
 	}),
 });
 
-export const collections = { blog, tweets };
+const diaries = defineCollection({
+	// Load Markdown files in the `src/content/diaries/` directory.
+	loader: glob({ base: './src/content/diaries', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		tags: z.array(z.string())
+	}),
+});
+
+const projects = defineCollection({
+	// Load Markdown files in the `src/content/projects/` directory.
+	loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		projectUrl: z.string().url().optional(),
+		repoUrl: z.string().url().optional(),
+	}),
+});
+
+export const collections = { blogs, tweets, diaries, projects };
