959694ee3f6d72c91c97b279b2bd91a77febd989
Ferry To <ferrywlto@gmail.com>
2025-10-18 19:08:36 +0100

    Fix: Individual tag page for blog posts (#54)
    * feat: Created TagCloud layout for the use of tag collection pages.
    
    * fix: Tag collection path in tag pill.
    
    * fix: Copilot comment about collection type.
    
    * fix: Font style for tag count in tag collection page.
    
    * fix: Links in tag pills in BlogPost
    
    * feat: Redesign the individual blog tag page.
    
    * refactor: Move TaggableCollection to types.ts
    
    * content: Update site description.
    
    * feat: Add name sort after count sort for tags.
    
    * fix:  Project tags link.
    
    * refactor: Use a share template page for all individual tag pages for all projects, blogs, diaries pages.
    
    * fix: Tag page header word casing.
    
    * fix: Very minor extra semicolon.

---
+++ src/types.ts
@@ -0,0 +1,41 @@
+import type { InferEntrySchema, RenderedContent } from "astro:content";
+
+export interface BlogPost {
+    id: string;
+    body?: string;
+    collection: "blogs";
+    data: InferEntrySchema<"blogs">;
+    rendered?: RenderedContent;
+    filePath?: string;
+}
+export interface DiaryPost {
+    id: string;
+    body?: string;
+    collection: "diaries";
+    data: InferEntrySchema<"diaries">;
+    rendered?: RenderedContent;
+    filePath?: string;
+}
+export interface ProjectPost {
+    id: string;
+    body?: string;
+    collection: "projects";
+    data: InferEntrySchema<"projects">;
+    rendered?: RenderedContent;
+    filePath?: string;
+}
+
+export type ContentPost = BlogPost | DiaryPost | ProjectPost;
+
+export type TaggableCollection = 'blogs' | 'diaries' | 'projects';
+
+export type ContentPathWithTag =
+{
+  params: {
+    collection: TaggableCollection;
+    tag: string;
+  };
+  props: {
+    posts: ContentPost[];
+  };
+}
