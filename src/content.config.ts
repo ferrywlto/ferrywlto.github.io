35a1d00e81ec716131930077b0dad0c22a06009f
Ferry To <ferrywlto@gmail.com>
2025-10-22 21:45:22 +0100

    feat: Hero image src in blog and dairy posts now support local image in-addition to remote url.

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,17 +5,16 @@ import { defineCollection, z, type SchemaContext } from 'astro:content';
 const CONTENT_BASE_PATH = './src/content';
 
 // Shared schema components
-const baseContentSchema = z.object({
+const buildBaseContentSchema = (ctx: SchemaContext) => z.object({
 	title: z.string(),
 	description: z.string(),
 	pubDate: z.coerce.date(),
 	updatedDate: z.coerce.date().optional(),
-  heroImage: z.string().optional(),
+  heroImage: z.union([ctx.image(), z.string().url()]).optional(),
   tags: z.array(z.string()),
 });
 
-
-const projectLinksSchema = (ctx: SchemaContext) => z.object({
+const buildProjectLinksSchema = (ctx: SchemaContext) => z.object({
 	projectUrl: z.string().url().optional(),
 	repoUrl: z.string().url().optional(),
   heroImage: z.union([ctx.image(), z.string().url()]).optional(),
@@ -32,17 +31,17 @@ const whispers = defineCollection({
 
 const blogs = defineCollection({
 	loader: glob({ base: `${CONTENT_BASE_PATH}/blogs`, pattern: '**/*.{md,mdx}' }),
-	schema: baseContentSchema,
+	schema: buildBaseContentSchema,
 });
 
 const diaries = defineCollection({
 	loader: glob({ base: `${CONTENT_BASE_PATH}/diaries`, pattern: '**/*.{md,mdx}' }),
-	schema: baseContentSchema,
+	schema: buildBaseContentSchema,
 });
 
 const projects = defineCollection({
 	loader: glob({ base: `${CONTENT_BASE_PATH}/projects`, pattern: '**/*.{md,mdx}' }),
-	schema: (ctx: SchemaContext) => baseContentSchema.merge(projectLinksSchema(ctx)),
+	schema: (ctx: SchemaContext) => buildBaseContentSchema(ctx).merge(buildProjectLinksSchema(ctx)),
 });
 
 export const collections = { blogs, whispers, diaries, projects };
